95b8ee60604e199e480c72bdcea0f0f41d910aee
john_16 <john_16@list.ru>
2016-07-09 12:31:33 +0300

    FIX MERGE

--- one.py
+++ one.py
@@ -12,7 +12,6 @@ print('2.5 commit. Final')
 print('3 commit')
 
 print('4 commit')
-<<<<<<< HEAD
 print('5 commit, 5')
 
 print('is a feature branch aliVe?')
@@ -20,7 +19,6 @@ print('is a feature branch aliVe?')
 print("another commit")
 
 print('prepare for conflicts, yep')
-=======
 print('5 commit, last')
 
 print('is a feature branch alive?')
@@ -28,5 +26,4 @@ print('is a feature branch alive?')
 print("another commit")
 
 print('prepare for conflicts and make conflict!')
->>>>>>> ae7e32e37016b72ff9df3e0c1af9bcea0c9064a5
 
