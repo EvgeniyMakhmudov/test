6aa0a635f0fcbd7b92ff8981781638dd4deaa520
john_16 <john_16@list.ru>
2016-07-09 12:37:16 +0300

    add functions

--- one.py
+++ one.py
@@ -2,6 +2,9 @@ print('init')
 
 print('1 commit')
 
+def square(value):
+    return value ** 2
+
 print('2 commit')
 print('2.1 commit')
 print('2.2 commit')
@@ -9,21 +12,6 @@ print('2.3 commit')
 print('2.4 commit')
 print('2.5 commit. Final')
 
-print('3 commit')
-
-print('4 commit')
-print('5 commit, 5')
-
-print('is a feature branch aliVe?')
-
-print("another commit")
-
-print('prepare for conflicts, yep')
-print('5 commit, last')
-
-print('is a feature branch alive?')
-
-print("another commit")
-
-print('prepare for conflicts and make conflict!')
+def mprint(value):
+    print(value)
 
