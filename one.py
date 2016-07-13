a9b1d92afd09750dc75d37223794219ee3586baa
john_16 <john_16@list.ru>
2016-07-13 22:28:56 +0300

    v1

--- one.py
+++ one.py
@@ -9,7 +9,4 @@ def mprint(value, end='\n'):
     print(value, end=end)
 
 if __name__ == '__main__':
-    mprint('Hello world!')
-
-if __name__ == '__main__':
-    mprint('Hello world!')
+    mprint('Hello world', end='!\n')
