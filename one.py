4985c695752795eecc09d720e17d6eeab36af48a
john_16 <john_16@list.ru>
2016-07-13 21:31:44 +0300

    some code clean

--- one.py
+++ one.py
@@ -1,16 +1,9 @@
 print('init')
 
-print('1 commit')
 
 def square(value):
     return value ** 2
 
-print('2 commit')
-print('2.1 commit')
-print('2.2 commit')
-print('2.3 commit')
-print('2.4 commit')
-print('2.5 commit. Final')
 
 def mprint(value):
     print(value)
