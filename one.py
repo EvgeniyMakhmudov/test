3cc0b54bee789d63945c4836c082c5050f5431ce
john_16 <john_16@list.ru>
2016-07-13 22:30:35 +0300

    v2

--- one.py
+++ one.py
@@ -1,4 +1,4 @@
-print('init')
+#!/usr/bin/env python3
 
 
 def square(value=1):
@@ -7,9 +7,6 @@ def square(value=1):
 
 def mprint(value, end='\n'):
     print(value, end=end)
-
-if __name__ == '__main__':
-    mprint('Hello world!')
 
 if __name__ == '__main__':
     mprint('Hello world!')
