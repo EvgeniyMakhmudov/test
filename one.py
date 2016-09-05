64f76028fcdd99a23ac0105e3f6a796a9dee3bc8
Evgeniy Makhmudov <john_16@list.ru>
2016-09-05 14:13:42 +0300

    New story

--- one.py
+++ one.py
@@ -1,27 +1,3 @@
 #!/usr/bin/env python3
 
-print('1')
-print('2')
-print('3')
-print('4')
-
-def square(value=1):
-    return value ** 2
-
-
-def mprint(value, end='\n'):
-    print(value, end=end)
-
-
-def foo():
-    print('Bar{}'.format(''))
-
-class Foo:
-    pass
-
-def mpypy():
-    print('My!my!!1')
-
-
-if __name__ == '__main__':
-    mprint('Hello world', end='!\n')
+print('hello')
